121fff44c83e2e585219d1811c241bf686276217
Liam Lotte <sergentzeug@gmail.com>
2023-11-17 13:25:19 +0100

    add exemple

--- array/main.c
+++ array/main.c
@@ -1,8 +1,16 @@
+/*
+Author : Zeug555
+Last Update : 17/11/23
+Title : main.c
+*/
+
 #include <stdio.h>
 #include "array.h"
 
 int main()
 {
+    /* --------------------------- Exemple ---------------------------*/
+
     //New Array Declaration
     printf("\n\n --- New Array Declaration --- \n");
     Array myArray = arrayInit(5,3);
@@ -29,14 +37,18 @@ int main()
 
     //Addition of 2 Arrays
     printf("\n\n --- Addition of 2 Arrays --- \n");
-    printf("\n\n array1 \n");
     Array myArrayAdd = arrayInit(5,3);
-    arrayModif(myArray, 1, 2, 3);
-    arrayModif(myArray, 2, 1, 1);
-    arrayModif(myArray, 1, 3, 4);
-    arrayModif(myArray, 2, 2, 5);
-    arrayModif(myArray, 0, 0, 3);
+    arrayModif(myArrayAdd, 1, 2, 1);
+    arrayModif(myArrayAdd, 0, 4, -1);
+    arrayModif(myArrayAdd, 2, 3, 4);
+    arrayModif(myArrayAdd, 0, 2, 5);
+    arrayModif(myArrayAdd, 0, 0, -3);
+    printf("\n-array1-\n");
+    arrayPrint(myArrayAdd);
+    printf("\n-array2-\n");
     arrayPrint(myArray);
+    printf("\n-Result-\n");
+    arrayPrint(arrayAdd(myArrayAdd, myArray));
 
     return 0;
 }
